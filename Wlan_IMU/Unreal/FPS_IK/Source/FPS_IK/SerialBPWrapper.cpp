b5c34dbee6e0d8a64e5b28dd2234ef4d3ffc4d31
Tobias Pretzl <tpretzl.mmt-m2015@fh-salzburg.ac.at>
2017-08-27 23:09:06 +0200

    ~ ups forgot too push other unreal project stuff

--- Wlan_IMU/Unreal/FPS_IK/Source/FPS_IK/SerialBPWrapper.cpp
+++ Wlan_IMU/Unreal/FPS_IK/Source/FPS_IK/SerialBPWrapper.cpp
@@ -10,85 +10,85 @@ void USerialBPWrapper::EndPlay(const EEndPlayReason::Type EndPlayReason)
 
 bool USerialBPWrapper::OpenComPort(FString ComPortName, int baudrate)
 {
-	//if (_serial)
+	////if (_serial)
+	////{
+	////	ClosePort();
+	////}
+
+	//try
 	//{
-	//	ClosePort();
+	//	std::string stdString(TCHAR_TO_UTF8(*ComPortName));
+	//	uint32_t baud = baudrate;
+
+	//	_serial = new serial::Serial(stdString, baud);
+
+	//	bool open = _serial->isOpen();
+	//	if (!open)
+	//	{
+	//		delete _serial;
+	//	}
+	//	return open;
 	//}
 
-	try
-	{
-		std::string stdString(TCHAR_TO_UTF8(*ComPortName));
-		uint32_t baud = baudrate;
-
-		_serial = new serial::Serial(stdString, baud);
-
-		bool open = _serial->isOpen();
-		if (!open)
-		{
-			delete _serial;
-		}
-		return open;
-	}
-
-	catch (const std::exception e)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("OpenComport -Expection: %s"), *FString(e.what()));
-		ClosePort();
+	//catch (const std::exception e)
+	//{
+	//	UE_LOG(LogTemp, Warning, TEXT("OpenComport -Expection: %s"), *FString(e.what()));
+	//	ClosePort();
 		return false;
 
-	}
+	//}
 }
 
 const FString lineBreak("\r\n");
 
 bool USerialBPWrapper::ReadLine(FString & line)
 {
-	try
-	{
-		line = "";
-		if (!_serial || _serial->available() < 1)
+	//try
+	//{
+	//	line = "";
+	//	if (!_serial || _serial->available() < 1)
 			return false;
 
 
-		std::string stdstr = _serial->readline();
+	//	std::string stdstr = _serial->readline();
 
-		line = UTF8_TO_TCHAR(stdstr.c_str());
-		line.RemoveFromEnd(lineBreak);
-		return true;
-	}
-	catch (std::exception& e)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("ReadLine - Expection: %s"), *FString(e.what()));
-		ClosePort();
-		return false;
+	//	line = UTF8_TO_TCHAR(stdstr.c_str());
+	//	line.RemoveFromEnd(lineBreak);
+	//	return true;
+	//}
+	//catch (std::exception& e)
+	//{
+	//	UE_LOG(LogTemp, Warning, TEXT("ReadLine - Expection: %s"), *FString(e.what()));
+	//	ClosePort();
+	//	return false;
 
-	}
+	//}
 }
 
 bool USerialBPWrapper::ClosePort()
 {
-	try
-	{
-		if (!_serial)
-			return false;
+	//try
+	//{
+	//	if (!_serial)
+	//		return false;
 
-		if (!_serial->isOpen())
-		{
-			delete _serial;
-			_serial = nullptr;
-			return false;
-		}
+	//	if (!_serial->isOpen())
+	//	{
+	//		delete _serial;
+	//		_serial = nullptr;
+	//		return false;
+	//	}
 
-		_serial->close();
-		delete _serial;
-		_serial = nullptr;
-		FOnCloseDelegate.Broadcast();
-		return true;
-	}
-	catch (const std::exception e)
-	{
-		UE_LOG(LogTemp, Warning, TEXT("Close Port - Expection: %s"), *FString(e.what()));
+	//	_serial->close();
+	//	delete _serial;
+	//	_serial = nullptr;
+	//	FOnCloseDelegate.Broadcast();
+	//	return true;
+	//}
+	//catch (const std::exception e)
+	//{
+	//	UE_LOG(LogTemp, Warning, TEXT("Close Port - Expection: %s"), *FString(e.what()));
 		return false;
 
-	}
+	//}
 }
