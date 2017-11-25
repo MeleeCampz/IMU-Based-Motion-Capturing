8394a0d723485f2581cd5d9866ff7d9229e11e24
Tobias Pretzl <tpretzl.mmt-m2015@fh-salzburg.ac.at>
2017-11-25 19:28:18 +0100

    ~ CustomForward animation node, now works as long as no bone is skipped in the chain (input rotations are treated as offset of curretn pose) ~ Disabled auto exposure in project settings

--- Wlan_IMU/Unreal/FPS_IK/Source/FPS_IK/Private/AnimNode_CustomForward.cpp
+++ Wlan_IMU/Unreal/FPS_IK/Source/FPS_IK/Private/AnimNode_CustomForward.cpp
@@ -19,34 +19,49 @@ void FAnimNode_CustomForward::EvaluateSkeletalControl_AnyThread(FComponentSpaceP
 		return;
 	
 	const FBoneContainer& BoneContainer = Output.Pose.GetPose().GetBoneContainer();
+	const FBoneContainer& BoneContainerUnModified = ForwardedPose.GetPose().GetBoneContainer();
 
+	//MAp to store applied rotations that we don't want to be applied to child bones
+	TMap<int32, FQuat> appliedRotaions;
+	
 	for (int i = 0; i < _bones.Num(); i++)
 	{
 		int rotIdx = BoneRotations.Names.IndexOfByKey(_bones[i].Key);
 		if (rotIdx == INDEX_NONE)
 			continue;
+
+		FQuat inRot = BoneRotations.Rotations[rotIdx];
 		
 		FCompactPoseBoneIndex index = _bones[i].Value.GetCompactPoseIndex(BoneContainer);
-		FTransform transform = Output.Pose.GetLocalSpaceTransform(index);
-		FQuat localRot = transform.GetRotation();
-
 		if (index == INDEX_NONE)
 			continue;
 
-
-		FCompactPoseBoneIndex parent = Output.Pose.GetPose().GetParentBoneIndex(index);
-		const FTransform& parentTM = Output.Pose.GetComponentSpaceTransform(parent);
+		//override rotaion with offset applied to input pose
+		FQuat oldRotInv = FQuat::Identity;
+		FCompactPoseBoneIndex curParent = Output.Pose.GetPose().GetParentBoneIndex(index);
 		
-		//calculate component space transform
-		transform *= parentTM; 
-		//override rotaion
-		transform.SetRotation(BoneRotations.Rotations[rotIdx]);
+		//Only works if we don't skip a bone in the animation chain....
+		while (curParent != INDEX_NONE)
+		{
+			FQuat* mapVal = appliedRotaions.Find(curParent.GetInt());
+			if (mapVal)
+			{
+				oldRotInv *= mapVal->Inverse();
+				break;	
+			}
+			curParent = Output.Pose.GetPose().GetParentBoneIndex(curParent);
+		}
+		
+		FTransform CSTransform = Output.Pose.GetComponentSpaceTransform(index);
+		FQuat compSpaceRot = CSTransform.GetRotation();
+		FQuat rotationOverride = compSpaceRot * inRot *oldRotInv;
+		appliedRotaions.Add(index.GetInt(), inRot);
+		CSTransform.SetRotation(rotationOverride);
 
-		//OutBoneTransforms.Add(FBoneTransform(index, originalLocalTM));
+		//OutBoneTransforms.Add(FBoneTransform(index, transform));
 		//Can be optimized.... Problem is that, any parent bones have to be updated bofore the children, or they willend up being disjointed, due to wrong positon
-		
 		TArray<FBoneTransform> TempTransforms;
-		TempTransforms.Add(FBoneTransform(FBoneTransform(index, transform)));
+		TempTransforms.Add(FBoneTransform(index, CSTransform));
 		Output.Pose.LocalBlendCSBoneTransforms(TempTransforms, 1.0f);
 		TempTransforms.Reset();
 	}
@@ -55,27 +70,21 @@ void FAnimNode_CustomForward::EvaluateSkeletalControl_AnyThread(FComponentSpaceP
 bool FAnimNode_CustomForward::IsValidToEvaluate(const USkeleton * Skeleton, const FBoneContainer & RequiredBones)
 {
 	for (TPair<FName, FBoneReference> boneRef : _bones)
-	{
 		if (!boneRef.Value.IsValidToEvaluate())
-		{
 			return false;
-		}
-	}
 
 	return true;
 }
 
 void FAnimNode_CustomForward::InitializeBoneReferences(const FBoneContainer & RequiredBones)
 {	
-	_bones.Empty();
-	
+	_bones.Empty();	
 	for (FName name : BoneRotations.Names)
 	{
 		FBoneReference bone = FBoneReference(name);
 		bone.Initialize(RequiredBones);
 		_bones.Add(TPairInitializer<FName, FBoneReference>(name, bone));
 	}
-
 	//Bones habe to be sorted in descendinf order, so paraent bones are updated before their childs
 	_bones.Sort([](const TPair<FName, FBoneReference> &a, const TPair<FName, FBoneReference> &b) { return a.Value.BoneIndex < b.Value.BoneIndex; })
 ;}
