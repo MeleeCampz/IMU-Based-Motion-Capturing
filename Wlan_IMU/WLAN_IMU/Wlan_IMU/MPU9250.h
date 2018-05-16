7540b7d17c4c5cba4d1424cb4f5253f6c429be01
Tobias Pretzl <tpretzl.mmt-m2015@fh-salzburg.ac.at>
2018-05-16 23:34:10 +0200

    - Increase magnetometer data read

--- Wlan_IMU/WLAN_IMU/Wlan_IMU/MPU9250.h
+++ Wlan_IMU/WLAN_IMU/Wlan_IMU/MPU9250.h
@@ -207,7 +207,7 @@ protected:
 	// Choose either 14-bit or 16-bit magnetometer resolution
 	uint8_t Mscale = MFS_16BITS;
 	// 2 for 8 Hz, 6 for 100 Hz continuous magnetometer data read
-	uint8_t Mmode = 0x02;
+	uint8_t Mmode = 0x06;
 
 public:
 
